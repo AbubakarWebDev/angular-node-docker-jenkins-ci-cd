a87aa595b2eb450bebeea9ffb69f7036b5e66a25
Muhammad Abubakar <90tricks90@gmail.com>
2023-02-12 23:46:22 +0500

    Update Error Handling

--- src/app/features/create/components/create-data/create-data.component.ts
+++ src/app/features/create/components/create-data/create-data.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
 import { ToastrService } from 'ngx-toastr';
+
 import { ApiEndpointsService } from 'src/app/core/services/api-endpoints.service';
 import { ApiHttpService } from 'src/app/core/services/api-http.service';
 
@@ -14,11 +17,11 @@ export class CreateDataComponent implements OnInit {
   constructor(
     private toastr: ToastrService,
     private apiHttpService: ApiHttpService,
-    private apiEndpointsService: ApiEndpointsService
+    private apiEndpointsService: ApiEndpointsService,
+    private router: Router,
   ) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit(userForm: NgForm): void {
     if (userForm.valid) {
@@ -33,19 +36,26 @@ export class CreateDataComponent implements OnInit {
       this.apiHttpService.post(
         this.apiEndpointsService.getUserEndpoint(),
         body
-      ).subscribe(res => {
-        if (res.code === 201) {
-          this.toastr.success('Your User Record Added Successfully!', 'User Added', {
-            timeOut: 3000,
-          });
-          userForm.reset();
-        }
-        else {
-          this.toastr.error('Some went wrong. please try again later!', 'Server Error', {
-            timeOut: 3000,
-          });
-        }
+      ).subscribe({
+        next: res => this.onSuccess(res, userForm),
+        error: err => this.onError(err)
       });
     }
+  }
+
+  onSuccess(res: any, userForm: NgForm) {
+    if (res.code === 201) {
+      this.toastr.success('Your User Record Added Successfully!', 'User Added', {
+        timeOut: 3000,
+      });
+      this.router.navigate(['/']);
+    }
+  }
+
+  onError(error: any) {
+    console.log(error);
+    this.toastr.error('Some went wrong. please try again later!', 'Server Error', {
+      timeOut: 3000,
+    });
   }
 }
